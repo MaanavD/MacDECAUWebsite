d702231b54ca53a4eba28e9d680cc7f3237a4b26
Maanav Dalal <Maanavdalal@gmail.com>
2019-08-25 23:03:28 -0400

    A few small changes, commented code removed

--- src/components/About.js
+++ src/components/About.js
@@ -4,17 +4,6 @@ import Carousel from './Carousel';
 function About() {
   return (
     <div className="About">
-      {/* <h1 className="about-title text-primary text-center">About Us</h1> */}
-      {/* <div className="container">
-        <p>
-          Wowow DECA is so cool! <br/>
-          McMaster does it best! <br/>
-          We've been going for x years <br/>
-          Please join our team!<br/>
-        </p>
-        <h2 className="about-title text-primary text-center">Our Main Events</h2>
-
-      </div> */}
       <Carousel/>
     </div>
   );
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,47 +2,51 @@ import React from 'react';
 
 function Carousel() {
   return (
-    <div className="bd-example">
-      <div id="carouselExampleCaptions" className="carousel slide carousel-fade" data-ride="carousel">
-        <ol className="carousel-indicators">
-          <li className="about-tabs active" data-target="#carouselExampleCaptions" data-slide-to="0"></li>
-          <li className="about-tabs" data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-          <li className="about-tabs" data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-        </ol>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src="https://via.placeholder.com/150x86" className="d-block w-100" alt="..."/>
-              <div className="carousel-caption d-none d-md-block">
-                <h5 className="text-primary">Chapter Meetings</h5>
-                <p className="text-primary">In meetings, we teach you about business cases and competition, with frequent guest speakers and opportunites to network.</p>
-              </div>
-        </div>
-            <div className="carousel-item">
-              <img src="https://via.placeholder.com/150x86" className="d-block w-100" alt="..."/>
-                <div className="carousel-caption d-none d-md-block">
-                  <h5 className="text-primary">MDCC</h5>
-                  <p className="text-primary">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-        </div>
-              <div className="carousel-item">
-                <img src="https://via.placeholder.com/150x86" className="d-block w-100" alt="..."/>
-                  <div className="carousel-caption d-none d-md-block">
-                    <h5 className="text-primary">Provincial Competition</h5>
-                    <p className="text-primary">Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                  </div>
-        </div>
-              </div>
-              <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                <span className="carousel-control-prev-icon " aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
-              </a>
-              <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
-              </a>
+    <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+      <ol className="carousel-indicators">
+        <li className="about-tabs active" data-target="#carouselExampleControls" data-slide-to="0"></li>
+        <li className="about-tabs" data-target="#carouselExampleControls" data-slide-to="1"></li>
+        <li className="about-tabs" data-target="#carouselExampleControls" data-slide-to="2"></li>
+      </ol>
+      <div className="carousel-inner bg-info" role="listbox">
+        <div className="carousel-item active">
+          <div className="carousel-image d-flex align-items-center justify-content-center min-vh-100">
+            <img src="https://nevadadeca.org/Websites/nevadadeca/images/BlogFeaturedImages/Screen%20Shot%202018-12-03%20at%207.00.11%20PM.png" className="d-block w-75 carousel-image" />
+            <div className="carousel-caption">
+              <h5 className="text-white">Chapter Meetings</h5>
+              <p className="text-white">In meetings, we teach you about business cases and competition, with frequent guest speakers and opportunites to network.</p>
             </div>
           </div>
-          );
-        }
-        
-        export default Carousel;
+        </div>
+        <div className="carousel-item">
+          <div className="carousel-image d-flex align-items-center justify-content-center min-vh-100">
+            <img src="https://nevadadeca.org/Websites/nevadadeca/images/BlogFeaturedImages/Screen%20Shot%202018-12-03%20at%207.00.11%20PM.png" className="d-block w-75 carousel-image" />
+            <div className="carousel-caption">
+              <h5>MDCC</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <div className="carousel-image d-flex align-items-center justify-content-center min-vh-100">
+            <img src="https://nevadadeca.org/Websites/nevadadeca/images/BlogFeaturedImages/Screen%20Shot%202018-12-03%20at%207.00.11%20PM.png" className="d-block w-75 carousel-image" />
+            <div className="carousel-caption">
+              <h5>Provincial Competition</h5>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
+      </a>
+      <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
+      </a>
+    </div>
+  );
+}
+
+export default Carousel;
