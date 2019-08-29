b1c7f42484a89f5b7f8493ef5e7738d37919333e
Maanav Dalal <Maanavdalal@gmail.com>
2019-08-29 18:18:38 -0400

    Updates

---
+++ precache-manifest.c19d68a0181274f6242597f8ab10739e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252b3e533c4351f96735db089f5b9a71",
+    "url": "/MacDECAUWebsite/index.html"
+  },
+  {
+    "revision": "ca80603d44359e5a2108",
+    "url": "/MacDECAUWebsite/static/css/main.a5dde0ac.chunk.css"
+  },
+  {
+    "revision": "c24873d6ef34b081be61",
+    "url": "/MacDECAUWebsite/static/js/2.926f43fe.chunk.js"
+  },
+  {
+    "revision": "ca80603d44359e5a2108",
+    "url": "/MacDECAUWebsite/static/js/main.1689b193.chunk.js"
+  },
+  {
+    "revision": "61080b156584ffc7e826",
+    "url": "/MacDECAUWebsite/static/js/runtime~main.11790fca.js"
+  },
+  {
+    "revision": "0d939cc809583f854cb932c398a25ff7",
+    "url": "/MacDECAUWebsite/static/media/MDCC.0d939cc8.png"
+  },
+  {
+    "revision": "807f6f0ff7485e125d4a8e1068125ea3",
+    "url": "/MacDECAUWebsite/static/media/bg.807f6f0f.png"
+  },
+  {
+    "revision": "b3b711c284d4ede0e614880e5ad078c2",
+    "url": "/MacDECAUWebsite/static/media/exec.b3b711c2.png"
+  },
+  {
+    "revision": "5cdeaa7945ae982f80c366e882626807",
+    "url": "/MacDECAUWebsite/static/media/logo.5cdeaa79.png"
+  },
+  {
+    "revision": "93261e76c8cf13f8822ac3c96cbbcc8c",
+    "url": "/MacDECAUWebsite/static/media/provs.93261e76.png"
+  },
+  {
+    "revision": "e77101d26b16732cac39f8dc9511fca9",
+    "url": "/MacDECAUWebsite/static/media/video.e77101d2.mp4"
+  }
+]);
